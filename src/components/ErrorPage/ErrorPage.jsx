51c0257c595a442da4e82fa13f428092a731a3e0
ALNoman78 <abdullaalno078@gmail.com>
2024-12-29 21:05:46 +0600

    projects setup completed

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h3>ErrorPage</h3>
+        </div>
+    )
+}
+
+ErrorPage.propTypes = {}
+
+export default ErrorPage
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+
+const Home = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+Home.propTypes = {}
+
+export default Home
