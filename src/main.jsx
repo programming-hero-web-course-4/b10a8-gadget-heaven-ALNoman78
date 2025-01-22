ef54b29aa575b169aa8863f8c3caf31327500090
ALNoman78 <abdullaalno078@gmail.com>
2025-01-22 20:28:55 +0600

    ad

--- src/main.jsx
+++ src/main.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
       {
         path: '/statistics',
         loader: () => fetch('/Data.json'),
-        // element: <div>moule</div>,
+        // element: <div>moule</div>,j
       },
       {
         path: '/product/:product_id',
